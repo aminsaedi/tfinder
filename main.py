afd28110716ec380f17f64b981f69b9b3fc23a20
Amin Saedi <amin.di.sam@gmail.com>
2023-06-09 02:43:57 -0400

    added openai model

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import aiohttp
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 from typing import Union
 from pyppeteer import launch
 
@@ -12,6 +15,8 @@ import os
 import uvicorn
 import time
 import requests
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
@@ -86,18 +91,62 @@ def run():
     return {"result": "done"}
 
 
+MAX_RESPONSE_TOKENS = 1000
+
+
+async def fetch_tweets(name: str):
+    params = {
+        "api_key": os.getenv("SCRAPER_API_KEY"),
+        "user_id": name.strip(),
+    }
+    tweets = []
+    result = requests.get(
+        "https://api.scraperapi.com/structured/twitter/v2/tweets", params=params)
+    data = result.json()
+    tweets.extend([tweet["text"] for tweet in data.get(
+        "tweets", []) if not tweet["text"].startswith("RT")])
+
+    params = {
+        "api_key": os.getenv("SCRAPER_API_KEY"),
+        "user_id": name.strip(),
+        "next_cursor": data.get("next_cursor")
+    }
+    result = requests.get(
+        "https://api.scraperapi.com/structured/twitter/v2/tweets", params=params)
+    data = result.json()
+    tweets.extend([tweet["text"] for tweet in data.get(
+        "tweets", []) if not tweet["text"].startswith("RT")])
+
+    return tweets[:2000]
+
+
+async def get_personality(name: str):
+    tweets = await fetch_tweets(name)
+    amin = "\n".join(tweets)[:2000]
+    prompt = f"متن های توییت کاربر توییتر به صورت زیر است. براساس این متن‌ها شخصیت اون رو با متد یونگی تحلیل کن. همچنین باید تخمین سن و جنسیت کاربر را هم گزارش دهی : \n {amin}"
+    max_tokens = MAX_RESPONSE_TOKENS
+    chat_completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "system", "content": prompt},
+                                         {"role": "user", "content": amin}
+                                         ], max_tokens=max_tokens)
+    response_text = chat_completion.choices[0].message.content
+    return response_text
+
+
 @app.post("/twitter")
 async def analyze_twitter_user(request: Request):
     data = await request.json()
-    username = data["name"]
 
-    result = requests.post(
-        "http://localhost:3000/twitter", json={"name": username}
-    )
+    os.system(
+        f"t whois {data['name']} | head -1 | tr -d ' ' | tr -d 'ID' > temp/whois.txt")
+    with open('temp/whois.txt', 'r') as file:
+        name = file.read().strip()
 
-    print(result.text)
+    result = await get_personality(name)
 
-    return result.text
+    print(result)
+
+    return result
 
     # return result.json()
 
